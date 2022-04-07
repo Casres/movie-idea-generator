a228f84c4c89a8ad7280fe33392ccd291fd19360
Casres <christian@casillas.tech>
2022-04-07 13:48:50 -0400

    SP/AskBCS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 
 // title cards
-var  topLeftCard = $("#c2"); 
+var topLeftCard = $("#text2"); 
 var topMiddleCard = $("#text0");
 var topRightCard = $("#text1");
 var bottomLeftCard = $("#text4");
@@ -17,9 +17,29 @@ var titleCardListener = function () {
     topLeftCard.on("click", function () {
         console.log("Top Left Card Clicked");
     });
+
+    topMiddleCard.on("click", function () {
+        console.log("Top Middle Card Clicked");
+    });
+
+    topRightCard.on("click", function () {
+        console.log("Top right Card Clicked");
+    });
+
+    bottomLeftCard.on("click", function () {
+        console.log("bottom Left Card Clicked");
+    });
+
+    bottomMiddleCard.on("click", function () {
+        console.log("Bottom Middle Card");
+    })
+
+    bottomRightCard.on("click", function () {
+        console.log("bottom right Card Clicked");
+    });
+
 };
 
-
-
+titleCardListener();
 
 // then takes its value and gives it to the next js file
